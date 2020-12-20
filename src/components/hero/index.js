731b62883168ab17efce8e15b138abac11459b4e
loreleim <lorelei.miyamura@gmail.com>
2020-12-19 19:16:20 -0800

    adding in a SVG hero image

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ReactComponent as HeroSVG } from "../../images/heroImage.svg";
 import style from "../hero/index.module.scss"
 
 export default class Hero extends Component {
@@ -12,6 +13,7 @@ export default class Hero extends Component {
       <div className={style.heroContainer}>
         <h2>Planning And Facilitating A Community Program Can Feel Intimidating,</h2>
         <p>and many of us don't feel like we're prepared. The following quiz can help you investigate which of the facilitation skills you already have, even if you've never thought about them before!</p>
+        <HeroSVG className={style.heroImage}></HeroSVG>
       </div>
     );
   }
