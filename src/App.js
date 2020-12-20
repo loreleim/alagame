8f7b090bf714ecff442b556e0dd23d0c98957875
loreleim <lorelei.miyamura@gmail.com>
2020-12-20 12:21:06 -0800

    rendering the footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/hero";
 import Quiz from "./components/quiz";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Home/>
       <Quiz/>
+      <Footer/>
     </div>
   );
 }
