6581fb133122925aedbda7872d8cf61f411752d3
loreleim <lorelei.miyamura@gmail.com>
2021-01-24 18:31:13 -0800

    conditional rendering when the start button is pressed

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import { ReactComponent as HeroSVG } from "../../images/heroImage.svg";
-import style from "../hero/index.module.scss"
+import style from "../hero/index.module.scss";
+import Quiz from "../quiz";
 
 export default class Hero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizShown: false, 
+    };
+  }
+
+  beginButton = () => {
+    this.setState({quizShown: true})
+  }
+
+  renderQuiz = () => {
+    return <Quiz/>
+  }
 
   render() {
+    const {quizShown} = this.state;
     return (
-      <div className={style.heroContainer}>
-        <div className={style.heroLeft}>
-          <h2>Do You Have What It Takes to Lead Conversations?</h2>
-          <p>Answer These 6 Questions to Find Out</p>
-          <button className={style.heroButton} onClick={()=> console.log("hello")}>Begin</button>
+      <div>
+        <div className={style.heroContainer}>
+          <div className={style.heroLeft}>
+            <h2>Do You Have What It Takes to Lead Conversations?</h2>
+            <p>Answer These 6 Questions to Find Out</p>
+            <button className={style.heroButton} onClick={()=> this.beginButton()}>Begin</button>
+          </div>
+          <HeroSVG className={style.heroImage}></HeroSVG>
         </div>
-        <HeroSVG className={style.heroImage}></HeroSVG>
+        {quizShown && this.renderQuiz()}
       </div>
     );
   }
