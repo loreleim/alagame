2e0c8e827c89e5b17811f0a480d582c72695b959
loreleim <lorelei.miyamura@gmail.com>
2021-01-24 18:29:27 -0800

    remove the quiz always rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Home from "./components/hero";
-import Quiz from "./components/quiz";
 import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Home/>
-      <Quiz/>
       <Footer/>
     </div>
   );
