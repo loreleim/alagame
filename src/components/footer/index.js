4d8ddc4859c01390058913552e827550b3df2a0c
loreleim <lorelei.miyamura@gmail.com>
2021-01-27 10:50:34 -0800

    Footer credits

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,6 +9,8 @@ export default class Footer extends Component {
       <footer>
         <div className={style.footerContainer}>
             <ALALogo className={style.alaLogo}></ALALogo>
+            <p>This interactive tool was developed for the American Library Association by Convergence Design Lab, LLC as part of the Libraries Transforming Communities: Facilitation Skills for Small and Rural Libraries,
+              a project made possible in part by the Institute of Museum and Library Services RE-17-19-0041-19.</p>
         </div>
       </footer>
     );
