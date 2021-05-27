5c29a9d2cf40228848c129ba71428b3bc9dbb225
loreleim <lorelei.miyamura@gmail.com>
2021-05-27 14:06:45 -0700

    initialize google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Home from "./components/hero";
 import Footer from "./components/footer";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=>{
+    ReactGA.pageview(window.location.pathname);
+  }, []);
   return (
     <div>
       <Home/>
